71f506501feb53865dc837648cec3a75ccdab957
Yoav <47596599+ChaoSpiritZ@users.noreply.github.com>
2020-01-01 13:41:40 +0200

    Add files via upload

---
+++ OOP 26 Homework/OOP 26 Homework/MyProtectedUniqueList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_26_Homework
+{
+    public class MyProtectedUniqueList
+    {
+        private List<string> words = new List<string>();
+        private string secretWord;
+
+        public MyProtectedUniqueList(string secretWord)
+        {
+            this.secretWord = secretWord;
+        }
+
+        public void Add(string word)
+        {
+            if (word == "" || word is null)
+                throw new ArgumentNullException();
+            if (words.Contains(word) == true)
+                throw new InvalidOperationException();
+            words.Add(word);
+        }
+
+        public void Remove(string word)
+        {
+            if (word == "" || word is null)
+                throw new ArgumentNullException();
+            if (words.Contains(word) == false)
+                throw new ArgumentException();
+            words.Remove(word);
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= words.Capacity)
+                throw new ArgumentOutOfRangeException();
+            words.RemoveAt(index);
+        }
+
+        public void Clear(string keycode)
+        {
+            if (keycode != secretWord)
+                throw new AccessViolationException();
+            words.Clear();
+        }
+
+        public void Sort(string keycode)
+        {
+            if (keycode != secretWord)
+                throw new AccessViolationException();
+            words.Sort();
+        }
+
+        public int ListCount()
+        {
+            return words.Capacity;
+        }
+
+        public override string ToString()
+        {
+            string listString = "Protected String List:";
+            words.ForEach(w => listString += $"\n{w}");
+            return listString;
+        }
+    }
+}
---
+++ OOP 26 Homework/OOP 26 Homework/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_26_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string codeWord = "peanut";
+            MyProtectedUniqueList pList = new MyProtectedUniqueList(codeWord);
+            pList.Add("shame");
+            pList.Add("cat");
+            pList.Add("door");
+            pList.Add("mystic");
+            Console.WriteLine(pList);
+            string existingWord = "door";
+            string toBeRemovedWord = "piano";
+            int toBeRemovedIndex = 4;
+            string wrongWord = "elephant";
+            try
+            {
+                //pList.Add(null);
+                //pList.Add("");
+                //pList.Add(existingWord);
+                //pList.Remove(toBeRemovedWord);
+                //pList.RemoveAt(toBeRemovedIndex);
+                pList.Clear(wrongWord);
+            }
+            catch (ArgumentNullException anex)
+            {
+                //Console.WriteLine(anex.Message);
+                Console.WriteLine("You can only input words that actually have characters!");
+            }
+            catch (ArgumentOutOfRangeException aoorex)
+            {
+                Console.WriteLine($"Removing failed! index [{toBeRemovedIndex}] is not within the list's bounds (0 - {pList.ListCount() - 1})");
+            }
+            catch (ArgumentException aex)
+            {
+                //Console.WriteLine(aex.Message);
+                Console.WriteLine($"removing failed! {toBeRemovedWord} was not found on the list! ");
+            }
+            catch(InvalidOperationException ioex)
+            {
+                //Console.WriteLine(ioex.Message);
+                Console.WriteLine($"The word '{existingWord}' already exists in the list!");
+            }
+            catch(AccessViolationException avex)
+            {
+                Console.WriteLine($"clearing failed! incorrect keycode! ({wrongWord})");
+            }
+        }
+    }
+}
